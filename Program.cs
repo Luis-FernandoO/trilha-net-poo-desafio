5070c95eaa8d854e9bed83bcd5e658e68f045397
luis <contatoluisfernandogaleano@gmail.com>
2024-09-30 08:15:21 -0400

    proejeto de celular pronto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("---------- Nokia -----------");
+Smartphone cel1 = new Nokia("897897897897", "Nokia G10","5456445454564", 256);
+cel1.Ligar();
+cel1.ReceberLigacao();
+cel1.InstalarAplicativo("Telegram");
+
+Console.WriteLine("---------- IPhone -----------");
+Smartphone cel2 = new Iphone("849848154124", "IPhone 16", "878787564532121", 512);
+cel2.Ligar();
+cel2.ReceberLigacao();
+cel2.InstalarAplicativo("Play Store");
